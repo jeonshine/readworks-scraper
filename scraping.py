fde070609d18bad8254b5d463cb0f90825908171
jeonshine <105637993+jeonshine@users.noreply.github.com>
2022-07-22 12:08:40 +0900

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait as wait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait as wait
+# from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as Soup
 import chromedriver_autoinstaller, time
 
